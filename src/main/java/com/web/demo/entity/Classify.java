0e2222eba30859dfdec569d8ffe1c947a8d0ac9d
w94nj <w94nj@DESKTOP-PGEBIJN>
2022-04-14 20:47:30 +0800

    配置資料表

---
+++ src/main/java/com/web/demo/entity/Classify.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.web.demo.entity;
+
+/**
+ *
+ * @author w94nj
+ */
+public class Classify {
+    
+}
