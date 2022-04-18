7b8124fff5601183408dadeea77eed6ded4bcb39
w94nj <w94nj@DESKTOP-PGEBIJN>
2022-04-18 22:20:00 +0800

    配置欄位

--- src/main/java/com/web/demo/entity/Sales.java
+++ src/main/java/com/web/demo/entity/Sales.java
@@ -5,10 +5,93 @@
  */
 package com.web.demo.entity;
 
+import java.util.Date;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
 /**
  *
  * @author w94nj
  */
-public class Sales {
+
+@Entity
+public class Sales implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column
+    private Integer price;
+    
+    @Column
+    private Integer quantity;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new Date();
+    
+    @ManyToOne
+    @JoinColumn(name = "pid",referencedColumnName = "id")
+    private Product product;
+    
+    @ManyToOne
+    private Employee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+    
     
 }
